d8929bf0a8110d7bd503cb14a3d7fa0f82886a8d
walchko <walchko@users.noreply.github.com>
2019-09-07 20:54:30 -0400

    up

--- node/server.js
+++ node/server.js
@@ -1,1 +1,28 @@
 #!/usr/bin/env node
+var msgpack = require('msgpack5')();
+var encode = msgpack.encode;
+// var decode = msgpack.decode;
+
+// const msg = {"a":[0.001,-2,3.1234]};
+// const a = encode(msg);
+//
+// console.log(msg);
+// console.log(a);
+// console.log(a.length);
+// console.log(decode(a));
+
+// pubber.js
+var zmq = require('zeromq')
+var sock = zmq.socket('pub');
+const url = new URL('tcp://127.0.0.1:3000');
+
+sock.bindSync(url.toString());
+console.log('Publisher bound to port 3000');
+
+setInterval(function(){
+    // console.log('sending a multipart message envelope');
+    const a = {"a":[0.001,-2,3.1234],"timestamp": Date.now()};
+    const msg = encode(a);
+    console.log("sending[",msg.length,"]:",a);
+    sock.send(msg);
+}, 500);
---
+++ node/sub.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var msgpack = require('msgpack5')();
+// var encode = msgpack.encode;
+var decode = msgpack.decode;
+
+// subber.js
+var zmq = require('zeromq');
+var sock = zmq.socket('sub');
+const url = new URL('tcp://127.0.0.1:3000');
+
+sock.connect(url.toString());
+sock.subscribe('');
+console.log('Subscriber connected to port 3000');
+
+sock.on('message', function(message) {
+    const msg = decode(message);
+    console.log('recv[', message.length, "]:", msg);
+});
