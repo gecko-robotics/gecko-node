edcf89080669db19af2353224c6246d508c0f5f7
walchko <walchko@users.noreply.github.com>
2019-09-08 09:47:01 -0400

    up

---
+++ node/db.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+"use strict";
+
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
